715a5e5692a4c599f6bc9ec625e533ab1f59b4c3
HuangYiQun <63465798+huangyiqun@users.noreply.github.com>
2026-07-29 09:30:36 +0800

    fix issue 4055 (#4743)

--- tests/test_alias_copy.py
+++ tests/test_alias_copy.py
@@ -30,3 +30,20 @@ def test_alias_copy(shape, dtype):
     with flag_gems.use_gems():
         res_out = torch.ops.aten.alias_copy(inp)
     utils.gems_assert_equal(res_out, ref_out)
+
+
+@pytest.mark.alias_copy_out
+@pytest.mark.parametrize("shape", utils.POINTWISE_SHAPES)
+@pytest.mark.parametrize("dtype", utils.FLOAT_DTYPES)
+def test_alias_copy_out(shape, dtype):
+    inp = torch.randn(shape, dtype=dtype, device=flag_gems.device)
+    ref_inp = utils.to_reference(inp)
+    ref_out = torch.empty_like(ref_inp)
+    out = torch.empty_like(inp)
+
+    torch.ops.aten.alias_copy(ref_inp, out=ref_out)
+    with flag_gems.use_gems():
+        res_out = torch.ops.aten.alias_copy(inp, out=out)
+
+    assert res_out is out
+    utils.gems_assert_equal(res_out, ref_out)
