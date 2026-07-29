005e826728f6912f8b4991c4dcc2fbe002b6475f
HuangYiQun <63465798+huangyiqun@users.noreply.github.com>
2026-07-29 09:30:43 +0800

    fix issue 4057 (#4742)

--- benchmark/test_cumsum.py
+++ benchmark/test_cumsum.py
@@ -23,6 +23,12 @@ def input_fn(shape, dtype, device):
     yield inp, 1
 
 
+def cumsum_out_input_fn(shape, dtype, device):
+    inp = utils.generate_tensor_input(shape, dtype, device)
+    out = torch.empty_like(inp)
+    yield inp, -1, {"out": out}
+
+
 @pytest.mark.cumsum
 def test_cumsum():
     bench = base.GenericBenchmark2DOnly(
@@ -30,6 +36,18 @@ def test_cumsum():
         op_name="cumsum",
         torch_op=torch.cumsum,
         dtypes=consts.FLOAT_DTYPES + consts.INT_DTYPES,
+    )
+
+    bench.run()
+
+
+@pytest.mark.cumsum_out
+def test_cumsum_out():
+    bench = base.GenericBenchmark2DOnly(
+        input_fn=cumsum_out_input_fn,
+        op_name="cumsum_out",
+        torch_op=torch.cumsum,
+        dtypes=consts.FLOAT_DTYPES,
     )
 
     bench.run()
