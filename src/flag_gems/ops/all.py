7ee15bb427df23b1a1c5dd1cc78d57201eab7981
Jinjie Liu <sgjzfzzf@gmail.com>
2026-08-20 22:53:01 +0800

    fix: validate dimensions in all_dims
    Signed-off-by: Jinjie Liu <sgjzfzzf@gmail.com>

--- src/flag_gems/ops/all.py
+++ src/flag_gems/ops/all.py
@@ -151,7 +151,8 @@ def all_dims(inp, dim=None, keepdim=False):
 
     if dim is None or isinstance(dim, int):
         return all_dim(inp, dim=dim, keepdim=keepdim)
-    assert ((i >= -inp.ndim and i < inp.ndim) for i in dim), "Invalid dim"
+    for i in dim:
+        assert i >= -inp.ndim and i < inp.ndim, "Invalid dim"
 
     shape = list(inp.shape)
     dim = [d % inp.ndim for d in dim]
