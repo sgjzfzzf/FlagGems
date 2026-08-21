3c49459c1b40005e0d625568a573e408c803d4db
SabYic <sunwenjia04@163.com>
2026-08-21 12:24:38 +0800

    modified:   src/flag_gems/ops/arange.py 	modified:   tests/test_arange_start.py

--- tests/test_arange_start.py
+++ tests/test_arange_start.py
@@ -36,9 +36,7 @@ device = flag_gems.device
         else utils.FLOAT_DTYPES + utils.ALL_INT_DTYPES + [None]
     ),
 )
-@pytest.mark.parametrize(
-    "device", [flag_gems.device] if cfg.QUICK_MODE else [flag_gems.device, None]
-)
+@pytest.mark.parametrize("device", [flag_gems.device])
 # Since triton only target to GPU, pin_memory only used in CPU tensors.
 @pytest.mark.parametrize("pin_memory", [False] if cfg.QUICK_MODE else [False, None])
 def test_arange(start, step, end, dtype, device, pin_memory):
@@ -51,9 +49,8 @@ def test_arange(start, step, end, dtype, device, pin_memory):
         pin_memory=pin_memory,
     )
 
-    with flag_gems.use_gems():
-        res_out = torch.arange(
-            start, end, step, dtype=dtype, device=device, pin_memory=pin_memory
-        )
+    res_out = flag_gems.ops.arange_start(
+        start, end, step, dtype=dtype, device=device, pin_memory=pin_memory
+    )
 
     utils.gems_assert_equal(res_out, ref_out)
